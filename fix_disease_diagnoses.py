78ae5ecd66a054ecf53dbb8f894cafe042e41906
user01 <2120171404@bit.edu.cn>
2019-11-15 15:02:18 +0800

    11月15日下午

---
+++ fix_disease_diagnoses.py
@@ -0,0 +1,146 @@
+# 导入库
+#import psycopg2
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+
+# 链接mimic
+'''
+def get_data_MIMIC(sql):
+    conn = psycopg2.connect(database='mimic',
+                            user='postgres',
+                            password='iamlog',
+                            host='127.0.0.1',
+                            port='5432')
+    cur = conn.cursor()
+    try:
+        cur.execute(sql)
+        #获取表的所有字段名称
+        coloumns = [row[0] for row in cur.description]
+        result = [[str(item) for item in row] for row in cur.fetchall()]
+        return pd.DataFrame(result,columns=coloumns)
+    except Exception as ex:
+        print(ex)
+    finally:
+        conn.close()
+'''
+# ----------------------诊断提取 11月14日
+# 
+# -- 统计特定疾病
+path = 'SET search_path to mimiciii;'
+query_diag1 ='''
+WITH disease AS (
+  SELECT na.hadm_id,COUNT(seq_num) AS icd_count FROM diagnoses_icd d
+  INNER JOIN z_hyperna_full_patients na
+  ON d.subject_id = na.subject_id AND d.hadm_id = na.hadm_id
+  AND icd9_code IN ( '''
+query_diag2 =''' )
+  GROUP BY na.hadm_id )
+SELECT 
+  --na.subject_id, na.hadm_id, na.icustay_id, -- icd_count
+  sum(CASE WHEN icd_count IS NOT NULL THEN 1 ELSE 0 END) AS diag
+FROM z_hyperna_full_patients na
+LEFT JOIN disease d
+ON d.hadm_id = na.hadm_id
+GROUP BY na.subject_id, na.hadm_id, na.icustay_id
+ORDER BY na.subject_id, na.hadm_id, na.icustay_id;'''
+query_disease_example = '''
+WITH diabetes AS (
+  SELECT na.hadm_id,COUNT(seq_num) AS icd_count FROM diagnoses_icd d
+  INNER JOIN z_hyperna_full_patients na
+  ON d.subject_id = na.subject_id AND d.hadm_id = na.hadm_id
+  AND icd9_code IN (SELECT icd9_code FROM d_icd_diagnoses WHERE LONG_TITLE LIKE '%Diabetes%' AND LONG_TITLE NOT LIKE '%diabetes insipidus%' AND LONG_TITLE NOT LIKE '%pregnancy%'
+  AND LONG_TITLE NOT LIKE '%Screening for diabetes mellitus%' AND LONG_TITLE NOT LIKE '%Neonatal diabetes mellitus%' AND LONG_TITLE NOT LIKE '%Family history%' )
+  GROUP BY na.hadm_id )
+SELECT 
+  --na.subject_id, na.hadm_id, na.icustay_id, -- icd_count
+  sum(CASE WHEN icd_count IS NOT NULL THEN 1 ELSE 0 END) AS diag
+FROM z_hyperna_full_patients na
+LEFT JOIN diabetes d
+ON d.hadm_id = na.hadm_id
+GROUP BY na.subject_id, na.hadm_id, na.icustay_id
+ORDER BY na.subject_id, na.hadm_id, na.icustay_id;'''
+
+
+query_diabetes =''' SELECT icd9_code FROM d_icd_diagnoses WHERE LONG_TITLE LIKE '%Diabetes%' AND LONG_TITLE NOT LIKE '%diabetes insipidus%' AND LONG_TITLE NOT LIKE '%pregnancy%'
+  AND LONG_TITLE NOT LIKE '%Screening for diabetes mellitus%' AND LONG_TITLE NOT LIKE '%Neonatal diabetes mellitus%' AND LONG_TITLE NOT LIKE '%Family history%' )
+  GROUP BY na.hadm_id '''
+
+
+query_cerebrovascular = ''' SELECT icd9_code FROM "d_icd_diagnoses" WHERE short_title ILIKE '%Cerebrovascular%' '''
+
+query_liver = ''' SELECT icd9_code FROM "d_icd_diagnoses" WHERE (short_title ILIKE '%liver%' and short_title not ilike '%deliver%' and long_title not ilike '%pregnancy%') '''
+
+query_Coronary = '''SELECT icd9_code FROM "d_icd_diagnoses" WHERE short_title ILIKE '%Coronary%' '''
+
+query_malignancy = '''SELECT icd9_code FROM "d_icd_diagnoses" WHERE short_title ILIKE '%malignancy%' '''
+
+query_dementia = '''SELECT icd9_code FROM "d_icd_diagnoses" WHERE short_title ILIKE '%dementia%' '''
+
+query_kidney = '''SELECT icd9_code FROM "d_icd_diagnoses"
+  WHERE (short_title ILIKE '%kidney%' and short_title not ilike '%study%' and short_title not ilike '%donor%' and short_title not ilike '%transplant%' and short_title not ilike '%exam%'
+  and short_title not ilike '%malignancy%' and long_title not ilike '%delivery%' and long_title not ilike '%abortion%')'''
+
+query_hyperlipidemia = '''SELECT icd9_code FROM d_icd_diagnoses WHERE LONG_TITLE LIKE '% hyperlipidemia%' OR LONG_TITLE LIKE '%hypercholesterolemia%' '''
+
+query_hypertension = '''SELECT icd9_code FROM d_icd_diagnoses WHERE LONG_TITLE LIKE '%hypertension%' AND LONG_TITLE NOT LIKE '%pregnancy%'
+  AND LONG_TITLE NOT LIKE '%childbirth%' AND LONG_TITLE NOT LIKE '%puerperium%' AND LONG_TITLE NOT LIKE '%without%'
+  AND LONG_TITLE NOT LIKE '%Portal hypertension%' AND LONG_TITLE NOT LIKE '%venous hypertension%' 
+  AND LONG_TITLE NOT LIKE '%Screening for hypertension%' AND LONG_TITLE NOT LIKE '%Family history%' '''
+
+query_pneumonia = '''SELECT icd9_code FROM d_icd_diagnoses WHERE LONG_TITLE LIKE '%pneumonia%' AND SHORT_TITLE NOT LIKE '%test%' AND SHORT_TITLE NOT LIKE '%exam%'
+  AND SHORT_TITLE NOT LIKE '%cult%'AND SHORT_TITLE NOT LIKE '%histo%' AND SHORT_TITLE NOT LIKE '%unspec%'AND SHORT_TITLE NOT LIKE '%Meth%'
+  AND SHORT_TITLE NOT LIKE '%Congenital%'AND SHORT_TITLE NOT LIKE '%Nd vac%'AND LONG_TITLE NOT LIKE '%vaccinaton%' '''
+
+query_diarrhea = '''SELECT icd9_code FROM d_icd_diagnoses WHERE LONG_TITLE LIKE '%diarrhea%' AND SHORT_TITLE NOT LIKE '%agt%' OR SHORT_TITLE LIKE'%diarrhea%'
+  AND SHORT_TITLE NOT LIKE '%Adv%' '''
+  
+query_infection = '''SELECT icd9_code FROM d_icd_diagnoses WHERE LONG_TITLE LIKE '%infection%' '''
+
+query_respiratoryfailure = '''SELECT icd9_code FROM d_icd_diagnoses WHERE LONG_TITLE LIKE '%respiratory failure%' '''
+
+query_sleepapnea = '''SELECT icd9_code FROM d_icd_diagnoses WHERE LONG_TITLE LIKE '%respiratory failure%' '''
+
+query_alkali = '''SELECT icd9_code FROM "mimiciii"."d_icd_diagnoses" WHERE "long_title" ILIKE '%alkali%'
+  AND LONG_TITLE NOT LIKE '%Alkaline chemical burn%' AND LONG_TITLE NOT LIKE '% therapeutic use%' '''
+
+query_Braininjury = '''SELECT icd9_code FROM d_icd_diagnoses WHERE "short_title" ILIKE '%Brain injury%'
+  AND LONG_TITLE NOT LIKE '%Personal history%' '''
+
+query_diabetesinsipidus = '''SELECT icd9_code FROM "d_icd_diagnoses" WHERE "long_title" ILIKE '%Diabetes insipidus%' '''
+
+query_burn = '''SELECT icd9_code FROM "d_icd_diagnoses" WHERE "long_title" ILIKE '%burn%' 
+  AND LONG_TITLE NOT LIKE '%Sunburn%'AND LONG_TITLE NOT LIKE '%friction burn%' '''
+
+query_adrenal = '''SELECT icd9_code FROM "d_icd_diagnoses" WHERE "long_title" ILIKE '%adrenal%' 
+  AND LONG_TITLE NOT LIKE '%neoplasm%' AND LONG_TITLE NOT LIKE '%newborn%'
+  AND LONG_TITLE NOT LIKE '%Injury%' AND LONG_TITLE NOT LIKE '%Meningococc%' '''
+  
+query_acidosis = '''SELECT icd9_code FROM "d_icd_diagnoses" WHERE "long_title" ILIKE '%acidosis%' 
+  AND LONG_TITLE NOT LIKE '%juvenile type%'AND LONG_TITLE NOT LIKE '%newborn%' '''
+  
+query_heartfailure = '''SELECT icd9_code FROM "d_icd_diagnoses" WHERE short_title ILIKE '%heart failure%' '''
+
+query_depression = '''SELECT icd9_code FROM "d_icd_diagnoses" WHERE "short_title" ILIKE '%depression%' 
+  AND LONG_TITLE NOT LIKE '%newborn%' '''
+
+
+
+name_diseases = ('diabetes','Coronary','magligtumor','cerebrovascular','liver',
+    'dementia','kidney','hyperlipidemia','hypertension','pneumonia',
+    'diarrhea','infection','respiratoryfailure','sleepapnea','Alkali',
+    'Braininjury','diabetesinsipidus','burn','adrenal','acidosis',
+    'heartfailure','depression')
+#diag = pd.DataFrame(columns=name_diseases)
+#diag[name_diseases[3]] = get_data_MIMIC(path+query_diag1+query_cerebrovascular+query_diag2)['diag']
+#diag[name_diseases[4]] = get_data_MIMIC(path+query_diag1+query_liver+query_diag2)['diag']
+
+
+for i in range(21):
+    name = 'query_'+ name_diseases[i]
+    print(path+eval(name))
+#    diag[name_diseases[i]] = get_data_MIMIC(path+query_diag1+ eval(name)+query_diag2)['diag']
+
+print(diag.head())
+print('debug')
+
--- forward_input_bolus.sql
+++ forward_input_bolus.sql
@@ -2,59 +2,37 @@
 -- labevents between charttime_na_high and charttime_last
 -- CREATE materialized VIEW labsfirstday AS
 SELECT
-  forw_in_bolus.subject_id, forw_in_bolus.hadm_id, forw_in_bolus.icustay_id
+  pvt.subject_id, pvt.hadm_id, pvt.icustay_id
 
-  , SUM(CASE WHEN label = 'BNP' THEN amount ELSE null END) as BNP
-  , SUM(CASE WHEN label = 'Uric Acid' THEN valuenum ELSE null END) as UricAcid
-  , AVG(CASE WHEN label = 'Neutrophils' THEN valuenum ELSE null END) as Neutrophils
-  , AVG(CASE WHEN label = 'CO2' THEN valuenum ELSE null END) as CO2
-  , AVG(CASE WHEN label = 'pO2' THEN valuenum ELSE null END) as pO2
+  , SUM(CASE WHEN colloid_bolus IS NOT NULL THEN colloid_bolus ELSE 0 END) as colloid_bolus
 
 FROM
 ( -- begin query that extracts the data
-  SELECT ie.subject_id, ie.hadm_id, ie.icustay_id
-  -- here we assign labels to ITEMIDs
-  -- this also fuses together multiple ITEMIDs containing the same data
-  , CASE
-        WHEN itemid IN (227446,225622,7294,50963) THEN 'BNP'
-        
-        -- amountuom ml       
-        WHEN itemid IN (SELECT itemid FROM inputevents_cv WHERE amountuom = 'ml')
-        AND IN () THEN 'itemname/ml'
-        /*
-        -- amountuom mg
-        WHEN itemid IN (SELECT itemid FROM inputevents_cv WHERE amountuom = 'ml'
-        AND 
-          ) THEN 'itemname/mg'
-          */
-
-
-        WHEN itemid IN (51256,51349,51232) THEN 'Neutrophils'
-        WHEN itemid IN (50818,3808,3810,50804) THEN 'CO2'
-        WHEN itemid IN (50821,50817) THEN 'pO2'
-        WHEN itemid IN (51006) THEN 'Urea Nitrogen'
-
-
-      ELSE null
-    END AS label
-  , -- add in some sanity checks on the values
-  -- the where clause below requires all valuenum to be > 0, so these are only upper limit checks
-    /*
-    CASE
-      WHEN itemid = 50862 and valuenum >    10 THEN null -- g/dL 'ALBUMIN'
-      WHEN itemid = 50868 and valuenum > 10000 THEN null -- mEq/L 'ANION GAP'
-      WHEN itemid = 51144 and valuenum <     0 THEN null -- immature band forms, %
-
-    ELSE le.valuenum
-    END AS valuenum
-    */
-    le.amount AS amount
-
+  SELECT ie.subject_id, ie.hadm_id, ie.icustay_id,le.colloid_bolus
   FROM z_hyperna_full_patients ie
 
   LEFT JOIN z_colloid_bolus le
     ON le.icustay_id = ie.icustay_id
-    AND le.charttime BETWEEN (ie.charttime_first ) AND (ie.charttime_na_high)
-) forw_in_bolus
-GROUP BY forw_in_bolus.subject_id, forw_in_bolus.hadm_id, forw_in_bolus.icustay_id
-ORDER BY forw_in_bolus.subject_id, forw_in_bolus.hadm_id, forw_in_bolus.icustay_id;
+    -- AND le.charttime BETWEEN (ie.charttime_first ) AND (ie.charttime_na_high)
+    AND le.charttime BETWEEN (ie.charttime_na_high ) AND (ie.charttime_last)
+) pvt
+GROUP BY pvt.subject_id, pvt.hadm_id, pvt.icustay_id
+ORDER BY pvt.subject_id, pvt.hadm_id, pvt.icustay_id;
+
+SELECT
+  pvt.subject_id, pvt.hadm_id, pvt.icustay_id
+
+  , SUM(CASE WHEN crystalloid_bolus IS NOT NULL THEN crystalloid_bolus ELSE 0 END) as crystalloid_bolus
+
+FROM
+( -- begin query that extracts the data
+  SELECT ie.subject_id, ie.hadm_id, ie.icustay_id,le.crystalloid_bolus
+  FROM z_hyperna_full_patients ie
+
+  LEFT JOIN z_crystalloid_bolus le
+    ON le.icustay_id = ie.icustay_id
+    -- AND le.charttime BETWEEN (ie.charttime_first ) AND (ie.charttime_na_high)
+    AND le.charttime BETWEEN (ie.charttime_na_high ) AND (ie.charttime_last)
+) pvt
+GROUP BY pvt.subject_id, pvt.hadm_id, pvt.icustay_id
+ORDER BY pvt.subject_id, pvt.hadm_id, pvt.icustay_id;
